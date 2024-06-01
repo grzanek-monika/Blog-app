cf3cf43dc5604358d250fc6e07e55c22e3efa388
Monika Grzanek <grzanekmonika@o2.pl>
2024-06-01 08:06:07 +0200

    Add data to initialState

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,5 +1,31 @@
 const initialState = {
-    posts: []
+    posts: [
+        {
+            id: '1',
+            title: 'Article title',
+            shortDescription: 'Short description of the article...',
+            content: 'Main content of the article',
+            publishedDate: '02-04-2024',
+            author: 'John Doe'
+      },
+        {
+            id: '2',
+            title: 'Article title II',
+            shortDescription: 'Short description of the article...',
+            content: 'Main content of the article',
+            publishedDate: '06-011-2023',
+            author: 'John Doe'
+      },
+        {
+            id: '3',
+            title: 'Article title III',
+            shortDescription: 'Short description of the article...',
+            content: 'Main content of the article',
+            publishedDate: '02-02-2023',
+            author: 'John Doe'
+      }
+      
+    ]
 }
 
 
